b7da1e6042f1f3253468d980191e1a3a3bdee6f4
Timothé BENOIT <timothe.benoit1@gmail.com>
2018-04-03 13:55:36 +0200

    Ajout des photos, textes et styles

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,37 @@ console.log('Welcome !');
 
 let name = 'Bastien';
 const user = new User(name);
+
+(($) => {
+
+  // Scroll animation speed
+  const speed = 400
+
+  // Cashing objects
+  const $root = $('html, body')
+  const $selector = $('a[href*="#"]:not([href="#"])')
+
+  $selector.on('click', function(event) {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+        location.hostname == this.hostname) {
+
+      // Prevent default link behavior
+      event.preventDefault()
+
+      // Store target object
+      let target = $(this.hash)
+      target = target.length ? target : $('[name=' + target.slice(1) + ']')
+
+      // Animate scroll
+      if (target.length) {
+        $root.animate({
+          scrollTop: target.offset().top
+        }, speed, () => {
+          // Update URL witch correct hash
+          window.location.hash = target['selector']
+        })
+      }
+    }
+  });
+
+})(jQuery)
